c2fc8d5a1d2b4cbd0214458ea033780d2432eee9
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-02 16:39:50 +0600

    fourd

---
+++ Data_type.py
@@ -0,0 +1,15 @@
+                # comment in tha python
+# comment in python-This is single line comment
+'''
+This is multiline comment.
+'''
+
+
+'''                          **Use of data type in python **
+
+ What is data type ?
+ - Data type is a characteristics of data. Who tells tha computer which data it can store.
+'''
+#There ar seven tpye of data in pyton languahge.
+# such that:
+# 1. Numerical type data:
