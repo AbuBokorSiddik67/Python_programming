5b36ed168b1414a118b627e6183663db86852ff8
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2024-11-04 08:42:37 +0600

    five

--- Data_type.py
+++ Data_type.py
@@ -13,3 +13,18 @@ This is multiline comment.
 #There ar seven tpye of data in pyton languahge.
 # such that:
 # 1. Numerical type data:
+# integer type data:
+#                 a=10;
+# float type data:
+#                 a=2.5;
+# complex type data:
+#                 a= 2+5j;
+# 2. Sequence type data:
+#string type data:
+# a="amr soner bangla ami tomai valo basi";
+#list type data:
+#my_list = [1,5,6,7];
+#tuple type data:
+# my_tuple=(1,two,3);
+# Range type data:
+# r= range(5);
